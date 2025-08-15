7237222981fb545b180cc9de976044bddcbbc771
Matthieu ELIE <91465747+MatthieuELIE@users.noreply.github.com>
2025-08-15 17:44:38 +0200

    Update Lualine config...

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,25 +1,17 @@
 return {
 	"nvim-lualine/lualine.nvim",
 	event = "VeryLazy",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
 	opts = {
 		options = {
 			theme = "catppuccin",
-			icons_enabled = true,
-			section_separators = { left = "", right = "" },
 			component_separators = { left = "", right = "" },
+			section_separators = { left = "", right = "" },
 			disabled_filetypes = {
-				statusline = {},
-				winbar = {},
-			},
-			ignore_focus = {},
-			always_divide_middle = true,
-			always_show_tabline = true,
-			globalstatus = false,
-			refresh = {
-				statusline = 100,
-				tabline = 100,
-				winbar = 100,
+				statusline = {
+					"alpha",
+					"dashboard",
+					"snacks_dashboard",
+				},
 			},
 		},
 		sections = {
@@ -29,49 +21,59 @@ return {
 				{
 					"diff",
 					colored = true,
-					symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
+					symbols = {
+						added = LazyVim.config.icons.git.added,
+						modified = LazyVim.config.icons.git.modified,
+						removed = LazyVim.config.icons.git.removed,
+					},
 				},
 			},
-			lualine_c = { "filename" },
+			lualine_c = {
+				{
+					LazyVim.lualine.pretty_path(),
+				},
+			},
 			lualine_x = {
 				{
 					function()
+						---@diagnostic disable-next-line: undefined-field
 						return require("noice").api.status.command.get()
 					end,
 					cond = function()
+						---@diagnostic disable-next-line: undefined-field
 						return package.loaded["noice"] and require("noice").api.status.command.has()
 					end,
 					color = function()
-						return { fg = Snacks.util.color("Statement") }
+						return {
+							fg = Snacks.util.color("Statement"),
+						}
 					end,
 				},
 				{
 					function()
+						---@diagnostic disable-next-line: undefined-field
 						return require("noice").api.status.mode.get()
 					end,
 					cond = function()
+						---@diagnostic disable-next-line: undefined-field
 						return package.loaded["noice"] and require("noice").api.status.mode.has()
 					end,
 					color = function()
-						return { fg = Snacks.util.color("Constant") }
+						return {
+							fg = Snacks.util.color("Constant"),
+						}
 					end,
 				},
-				"filetype",
+				{
+					"filetype",
+					icon_only = false,
+					separator = "",
+					padding = {
+						left = 0,
+						right = 1,
+					},
+				},
 			},
-			lualine_y = { "progress" },
-			lualine_z = { "location" },
 		},
-		inactive_sections = {
-			lualine_a = {},
-			lualine_b = {},
-			lualine_c = { "filename" },
-			lualine_x = { "location" },
-			lualine_y = {},
-			lualine_z = {},
-		},
-		tabline = {},
-		winbar = {},
-		inactive_winbar = {},
-		extensions = { "fugitive" },
 	},
 }
