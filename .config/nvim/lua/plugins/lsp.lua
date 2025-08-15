17006da596c796e3d20c17337c677e9019053f94
Matthieu ELIE <elie.matthieu2@gmail.com>
2025-08-15 13:09:54 +0200

    Remove useless code

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -15,14 +15,7 @@ return {
 		"neovim/nvim-lspconfig",
 		opts = {
 			servers = {
-				ts_ls = {
-					root_dir = require("lspconfig").util.root_pattern({ "package.json", "tsconfig.json" }),
-					single_file_support = false,
-					settings = {},
-				},
 				denols = {
-					root_dir = require("lspconfig").util.root_pattern({ "deno.json", "deno.jsonc" }),
-					single_file_support = false,
 					init_options = {
 						enable = true,
 						lint = true,
