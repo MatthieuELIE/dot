077b310c08c7151c72a27ab2b5611c0353b6c385
Matthieu ELIE <elie.matthieu2@gmail.com>
2025-09-15 14:07:36 +0200

    Update Flash/Noice config...

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -7,35 +7,36 @@ return {
 	cmd = { "ConformInfo" },
 	---@module "conform"
 	---@type conform.setupOpts
-	opts = (function()
-		return {
-			-- Map filetypes to formatters to run on those filetypes
-			formatters_by_ft = {
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				lua = { "stylua" },
-				css = { "prettier" },
-				html = { "prettier" },
-				json = { "prettier" },
-				jsonc = { "prettier" },
-				markdown = { "prettier" },
-				sh = { "shfmt" },
-				xslt = { "xmlformatter" },
-				xml = { "xmlformatter" },
-				java = { "jdlts" },
-			},
-			-- Custom formatter options
-			formatters = {
-				shfmt = {
-					-- Arguments to pass to shfmt: indent with 4 spaces and use spaces instead of tabs
-					prepend_args = { "-i", "4", "-s" },
-				},
-			},
-			-- Default formatting options
-			default_format_opts = {
-				-- Use LSP formatter only as a fallback if no formatter is available
-				lsp_format = "fallback",
-			},
-		}
-	end)(),
+	opts = {
+		-- Map filetypes to formatters to run on those filetypes
+		formatters_by_ft = {
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			lua = { "stylua" },
+			css = { "prettier" },
+			html = { "prettier" },
+			json = { "prettier" },
+			jsonc = { "prettier" },
+			markdown = { "prettier" },
+			sh = { "shfmt" },
+			xslt = { "lemminx" },
+			xml = { "lemminx" },
+			java = { "google-java-format" },
+		},
+		-- Custom formatter options
+		formatters = {
+			-- ["google-java-format"] = {
+			-- 	args = {
+			-- 		"--aosp",
+			-- 		"--length=120",
+			-- 		"--replace",
+			-- 	},
+			-- },
+		},
+		-- Default formatting options
+		default_format_opts = {
+			-- Use LSP formatter only as a fallback if no formatter is available
+			lsp_format = "fallback",
+		},
+	},
 }
