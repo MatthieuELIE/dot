b855794fcf6d6b441a566878d2a41e870b4fbade
Matthieu ELIE <elie.matthieu2@gmail.com>
2026-05-25 14:33:00 +0200

    fix lualine theme

--- .config/nvim/plugin/lualine.lua
+++ .config/nvim/plugin/lualine.lua
@@ -1,5 +1,6 @@
 require('lualine').setup({
     options = {
+        theme = 'catppuccin-nvim',
         component_separators = { left = '', right = '' },
         section_separators = { left = '', right = '' },
     },
