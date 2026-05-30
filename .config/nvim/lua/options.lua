7226d84c43b65044df10bedd03bbc19035bb4982
Matthieu ELIE <elie.matthieu2@gmail.com>
2026-05-30 10:23:07 +0200

    fix options

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,28 +1,35 @@
+-- Leader key
 vim.g.mapleader = ' '
 
+-- Clipboard
 vim.opt.clipboard = vim.env.SSH_CONNECTION and '' or 'unnamedplus'
 
+-- Indentation
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
-
 vim.opt.smartindent = true
 
+-- Search
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
+-- Line numbers and UI
 vim.opt.number = true
 vim.opt.relativenumber = true
-
+vim.opt.cursorline = true
 vim.opt.termguicolors = true
-
-vim.opt.inccommand = 'split'
-
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
-vim.o.winborder = 'rounded'
-
 vim.o.signcolumn = 'yes'
 
+-- Command and search
+vim.opt.inccommand = 'split'
+
+-- Windows and layout
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.o.winborder = 'rounded'
+vim.opt.laststatus = 3
+
+-- Editing and completion
 vim.opt.undofile = true
+vim.opt.completeopt = 'menu,menuone,noselect'
--- .config/nvim/plugin/telescope.lua
+++ .config/nvim/plugin/telescope.lua
@@ -9,20 +9,24 @@ require('telescope').setup({
                 height = 0.9,
             },
         },
+
         file_ignore_patterns = {
             '%.git/',
             'node_modules/',
         },
     },
+
     pickers = {
         find_files = {
             hidden = true,
         },
+
         live_grep = {
             additional_args = function()
                 return { '--hidden' }
             end,
         },
+
         grep_string = {
             additional_args = function()
                 return { '--hidden' }
