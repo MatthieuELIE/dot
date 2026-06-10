814579e3e456e94dee517364efd3859e9e5a2650
Matthieu ELIE <matthieu-externe.elie@edf.fr>
2026-06-10 11:52:25 +0200

    add catppuccin to telescope

--- .config/nvim/plugin/catppuccin.lua
+++ .config/nvim/plugin/catppuccin.lua
@@ -1,7 +1,21 @@
 require('catppuccin').setup({
     flavour = 'mocha',
     transparent_background = true,
-    auto_integrations = true,
+
+    custom_highlights = function(colors)
+        return {
+            TelescopeNormal = { bg = 'none' },
+            TelescopeBorder = { bg = 'none' },
+
+            TelescopePromptNormal = { bg = 'none' },
+            TelescopeResultsNormal = { bg = 'none' },
+            TelescopePreviewNormal = { bg = 'none' },
+
+            TelescopePromptTitle = { bg = 'none', fg = colors.red },
+            TelescopeResultsTitle = { bg = 'none', fg = colors.green },
+            TelescopePreviewTitle = { bg = 'none', fg = colors.mauve },
+        }
+    end,
 })
 
 vim.cmd.colorscheme('catppuccin')
