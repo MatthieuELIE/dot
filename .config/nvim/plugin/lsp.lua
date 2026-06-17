be2458cd335e18de0f6070fd37d3606c99f03e5c
Matthieu ELIE <elie.matthieu2@gmail.com>
2026-06-17 18:15:28 +0200

    update eslint lsp config

--- .config/nvim/plugin/lsp.lua
+++ .config/nvim/plugin/lsp.lua
@@ -34,13 +34,22 @@ vim.lsp.config('eslint', {
     capabilities = capabilities,
     cmd = { 'vscode-eslint-language-server', '--stdio' },
     filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
-    root_markers = { '.eslintrc', '.eslintrc.js', '.eslintrc.json', 'eslint.config.js', 'package.json', '.git' },
+    root_dir = function(path)
+        return vim.fs.root(path, {
+            '.eslintrc.js',
+            '.eslintrc.json',
+            'eslint.config.js',
+            'eslint.config.ts',
+        })
+    end,
     settings = {
-        validate = 'on',
-        rulesCustomizations = {},
-        run = 'onType',
-        nodePath = '',
-        workingDirectory = { mode = 'auto' },
+        eslint = {
+            validate = 'on',
+            rulesCustomizations = {},
+            run = 'onType',
+            nodePath = '',
+            workingDirectories = { mode = 'auto' },
+        },
     },
 })
 
