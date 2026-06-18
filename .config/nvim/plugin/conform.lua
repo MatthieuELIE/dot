0b994c8a1a8d2976292d3e39a0129588831611b9
Matthieu ELIE <elie.matthieu2@gmail.com>
2026-06-18 18:07:23 +0200

    update conform config

--- .config/nvim/plugin/conform.lua
+++ .config/nvim/plugin/conform.lua
@@ -1,8 +1,11 @@
 require('conform').setup({
     formatters_by_ft = {
+        javascript = { 'prettier' },
         lua = { 'stylua' },
-        rust = { 'rustfmt' },
         markdown = { 'prettier' },
+        rust = { 'rustfmt' },
+        typescript = { 'prettier' },
+        vue = { 'prettier' },
     },
     format_on_save = {
         timeout_ms = 500,
